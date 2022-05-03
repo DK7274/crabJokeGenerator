124d37e2be74a86e6029aee5ad6900f382368b40
Darcy Keys <dkeys8722@gmail.com>
2022-05-03 11:03:50 +1000

    added more crimes

--- main.py
+++ main.py
@@ -3,14 +3,23 @@ import random
 app = App("CrabJokes")
 app.bg = "red"
 punchDict = {
-    1: "crimes",
-    2: "murder",
-    3: "crabs",
-    4: "he broke the claw",
-    5:"he had a knife"
+    1: "Crimes",
+    2: "Murder",
+    3: "Crabs",
+    4: "He broke the claw",
+    5:"He had a knife",
+    6:"I put him there",
+    7:"International drug smuggling",
+    8:"Aggravated assault",
+    9:"Money Laundering",
+    10:"Fraud",
+    11:"Because it didn't",
+    12:"Homicide",
+    13:"Tax Evasion",
+    14:"Viewed moose from the air"
 }
 def crabGen():
-    joke = Text(app, text="Why did the crab go to jail? " + punchDict[random.randint(1,5)])
+    joke = Text(app, text="Why did the crab go to jail? " + punchDict[random.randint(1,14)])
     print("amogus")
 message = Text(app,text="Welcome to crab jokes generator!")
 button = PushButton(app,text="generate crab joke",command = crabGen)
